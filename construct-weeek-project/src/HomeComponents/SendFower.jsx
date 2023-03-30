98332fa8789d1ffc4b1caca433fd22249ee33afe
sourabh7909 <112754573+sourabh7909@users.noreply.github.com>
2023-03-30 21:12:17 +0530

    day3 code

--- construct-weeek-project/src/HomeComponents/SendFower.jsx
+++ construct-weeek-project/src/HomeComponents/SendFower.jsx
@@ -1,9 +1,11 @@
 import { Box ,Center,SimpleGrid,Image,Text} from '@chakra-ui/react'
 import { folwers } from '../Getdata/Axios'
 import {useState,useEffect} from "react"
+import { useNavigate } from 'react-router-dom'
 
 const SendFower = () => {
     const [navtop, setnavtop] = useState([])
+    const navigate=useNavigate()
     const fetchandupdatedata = async () => {
         try {
             await folwers()
@@ -22,7 +24,7 @@ const SendFower = () => {
                 <SimpleGrid w={'92%'} columns={6} gap={15}>
                     {navtop.map((el) =>
                           
-                        <Box overflow={'hidden'} key={el.id}>
+                        <Box cursor={'pointer'} onClick={()=>navigate('/productpage')} overflow={'hidden'} key={el.id}>
                             <Image  overflow={'hidden'} _hover={{transform:'scale(1.07)'}} transition={'all 0.3s'} cursor={'pointer'} src={el.image} w={'100%'} alt={el.id} ></Image>
                            <Center> <Text mt={1} textAlign={'center'} fontWeight={'semibold'} fontSize={'1.1em'} color={'rgb(31, 31, 27)'}>{el.name}</Text></Center>
                         </Box>
---
+++ construct-weeek-project/src/Pages/ProductHomepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../HomeComponents/Navbar'
+import {Box} from "@chakra-ui/react"
+
+const ProductHomepage = () => {
+  return (
+    <Box>
+     <Navbar/>
+    </Box>
+  )
+}
+
+export default ProductHomepage
